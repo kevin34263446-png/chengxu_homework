61bb5e95055f8884655661b40f39332c1040f0ba
kevin34263446-png <keevin34263446@gmail.com>
2026-06-04 12:57:14 +0800

    修改从键盘读取并显示功能

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # chengxu_homework
+
+1.git commit -m "添加 main.c 文件
+
+2.修改从键盘读取并显示文字
--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include<stdlib.h>
 int main()
 {
+    int a;
+    char str[100];
+    scanf("%d", &a);
+    scanf("%s", str);
+    printf("%d\n", a);
+    printf("%s\n", str);
     printf("hello world\n");
     return 0;
 }
