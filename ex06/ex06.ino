32388eaa498428f0229971fc3d9ae9357b2ddf31
kevin34263446-png <keevin34263446@gmail.com>
2026-06-28 15:30:27 +0800

    ex06-2: 加入第二LED通道，双通道PWM同相渐变

--- ex06/ex06.ino
+++ ex06/ex06.ino
@@ -1,23 +1,29 @@
-// ex06.ino Step1 — 单LED PWM渐变闪烁
+// ex06.ino Step2 — 双LED双通道PWM（同相渐变）
 const int ledPinA = 2;
+const int ledPinB = 4;
 const int freq = 5000;
 const int channelA = 0;
+const int channelB = 1;
 const int resolution = 8;
 
 void setup() {
   ledcSetup(channelA, freq, resolution);
+  ledcSetup(channelB, freq, resolution);
   ledcAttachPin(ledPinA, channelA);
+  ledcAttachPin(ledPinB, channelB);
 }
 
 void loop() {
-  // 渐亮
+  // 两个LED同时渐亮
   for (int duty = 0; duty <= 255; duty++) {
     ledcWrite(channelA, duty);
+    ledcWrite(channelB, duty);
     delay(5);
   }
-  // 渐暗
+  // 两个LED同时渐暗
   for (int duty = 255; duty >= 0; duty--) {
     ledcWrite(channelA, duty);
+    ledcWrite(channelB, duty);
     delay(5);
   }
 }
